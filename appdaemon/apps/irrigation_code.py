a038fe9791c0c0517eb27063c41f69ed5110981d
Dave-Bryant <davidcolinbryant@gmail.com>
2020-12-08 13:38:02 +1100

    Added Garden Sprinklers

--- appdaemon/apps/irrigation_code.py
+++ appdaemon/apps/irrigation_code.py
@@ -5,6 +5,7 @@ class Home_Irrigation(hass.Hass):
 
   def initialize(self):
      self.start_time = self.args["START_TIME"]
+     self.start_days = self.args["START_DAYS"]
      self.reset_backet = self.args["RESET_BUCKET"]
      self.no_of_schedules = self.args["NO_OF_SCHEDULES"]
      self.master_valve_lead_time = self.args["MASTER_VALVE_LEAD_TIME"]
@@ -13,18 +14,24 @@ class Home_Irrigation(hass.Hass):
      self.station2 = self.args["STATION_2"]
      self.station3 = self.args["STATION_3"]
      self.station4 = self.args["STATION_4"]
+     self.station5 = self.args["STATION_5"]
+     self.station6 = self.args["STATION_6"]
      self.station1_weight = self.args["STATION_1_WEIGHT"]
      self.station2_weight = self.args["STATION_2_WEIGHT"]
      self.station3_weight = self.args["STATION_3_WEIGHT"]
      self.station4_weight = self.args["STATION_4_WEIGHT"]
+     self.station5_weight = self.args["STATION_5_WEIGHT"]
+     self.station6_weight = self.args["STATION_6_WEIGHT"]
      self.window1 = self.args["STATION_1_WINDOW"]
      self.window2 = self.args["STATION_2_WINDOW"]
      self.window3 = self.args["STATION_3_WINDOW"]
+     self.window4 = self.args["STATION_4_WINDOW"]
+     self.window5 = self.args["STATION_5_WINDOW"]
 
-     self.run_daily(self.main_routine, self.start_time)
-     #self.run_in(self.main_routine, 0)
+     self.run_daily(self.main_routine, self.start_time, constrain_days = self.start_days)
+     # self.run_in(self.main_routine, 0)
 
-  def main_routine(self, *args):     # change daily to > 60 to stop small runs
+  def main_routine(self, *args):
      self.running_time = self.render_template("{{states('sensor.smart_irrigation_daily_adjusted_run_time') | int}}")
      self.log(f"Daily is: {self.running_time} seconds. Hourly is: {int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time_2'))} seconds. ")
      if int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time_2')) > 0 and self.running_time > 0:
@@ -34,39 +41,65 @@ class Home_Irrigation(hass.Hass):
          self.station2_running_time = self.running_time*self.station2_weight
          self.station3_running_time = self.running_time*self.station3_weight
          self.station4_running_time = self.running_time*self.station4_weight
-         self.log(f"Station running times (minutes): Station 1: {self.station1_running_time/60:.2f} Station 2: {self.station2_running_time/60:.2f} Station3: {self.station3_running_time/60:.2f} Station4: {self.station4_running_time/60:.2f}")
+         self.station5_running_time = self.running_time*self.station5_weight
+         self.station6_running_time = self.running_time*self.station6_weight
+         self.log(f"Station running times (minutes): Station 1: {self.station1_running_time/60:.2f} Station 2: {self.station2_running_time/60:.2f} Station3: {self.station3_running_time/60:.2f} Station4: {self.station4_running_time/60:.2f} Station5: {self.station5_running_time/60:.2f} Station6: {self.station6_running_time/60:.2f}")
          # make sure all valves are off
-         self.turn_off(self.station1)
-         self.turn_off(self.station2)
-         self.turn_off(self.station3)
-         self.turn_off(self.station4)
+         if self.station1 != '': self.turn_off(self.station1)
+         if self.station2 != '': self.turn_off(self.station2)
+         if self.station3 != '': self.turn_off(self.station3)
+         if self.station4 != '': self.turn_off(self.station4)
+         if self.station5 != '': self.turn_off(self.station5)
+         if self.station6 != '': self.turn_off(self.station6)
 
          # Turn on first station after waiting self.master_valve_lead_time seconds
-         self.run_in(self.turn_on_station_cb, self.master_valve_lead_time, current_station = self.station1)
-         self.running_time = self.station1_running_time + self.master_valve_lead_time
-         self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station1)
+         if self.station1 != '':
+             self.running_time = self.master_valve_lead_time
+             self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station1)
+             self.running_time = self.station1_running_time + self.running_time
+             self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station1)
+         else: self.window1 = 0
 
-         self.running_time = self.window1 + self.valve_lead_time
-         self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station2)
-         self.running_time = self.station2_running_time + self.running_time
-         self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station2)
+         if self.station2 != '':
+             self.running_time = self.window1 + self.valve_lead_time
+             self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station2)
+             self.running_time = self.station2_running_time + self.running_time
+             self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station2)
+         else: self.window2 = 0
 
-         self.running_time = self.window1 + self.window2 + self.valve_lead_time
-         self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station3)
-         self.running_time = self.station3_running_time + self.running_time
-         self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station3)
+         if self.station3 != '':
+             self.running_time = self.window1 + self.window2 + self.valve_lead_time
+             self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station3)
+             self.running_time = self.station3_running_time + self.running_time
+             self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station3)
+         else: self.window3 = 0
 
-         self.running_time = self.window1 + self.window2 + self.window3 + self.valve_lead_time
-         self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station4)
-         self.running_time = self.station4_running_time + self.running_time
-         self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station4)
+         if self.station4 != '':
+             self.running_time = self.window1 + self.window2 + self.window3 + self.valve_lead_time
+             self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station4)
+             self.running_time = self.station4_running_time + self.running_time
+             self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station4)
+         else: self.window4 = 0
+
+         if self.station5 != '':
+             self.running_time = self.window1 + self.window2 + self.window3 + self.window4 + self.valve_lead_time
+             self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station5)
+             self.running_time = self.station5_running_time + self.running_time
+             self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station5)
+         else: self.window5 = 0
+
+         if self.station6 != '':
+             self.running_time = self.window1 + self.window2 + self.window3 + self.window4 + self.window5 + self.valve_lead_time
+             self.run_in(self.turn_on_station_cb, self.running_time, current_station = self.station6)
+             self.running_time = self.station6_running_time + self.running_time
+             self.run_in(self.turn_off_station_cb, self.running_time, current_station = self.station6)
 
          if self.reset_backet:
              self.call_service("smart_irrigation/smart_irrigation_reset_bucket", entityid = "sensor.smart_irrigation_bucket")
          self.log("Irrigation schedule set")
 
      else:
-         self.log("Irrigation not needed")         
+         self.log("Irrigation not needed")
 
 # Methods
 
