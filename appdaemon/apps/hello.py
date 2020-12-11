faad67c70c18e2e40a039f76ce26e45f3802026c
Dave-Bryant <davidcolinbryant@gmail.com>
2020-12-11 13:23:53 +1100

    PWS talking to HA

--- appdaemon/apps/hello.py
+++ appdaemon/apps/hello.py
@@ -23,7 +23,7 @@ class HelloWorld(hass.Hass):
       self.log("argument1 is: %s", argument1)
       self.log("Hello from test function %s and %s", argument1, match)
       self.station1_running_time = 0.0001
-      self.log(f"Station running times (minutes): Station 1: {self.station1_running_time/60:.2f}")
+      self.log(f"Testing running times (minutes): Tester 1: {self.station1_running_time/60:.2f}")
       #if self.entities.switch.tasmota.state == 'off':  # check for none
           #self.turn_on('switch.tasmota')
       self.log("Testing 1 2 3: %s", self.entities.switch.frlawnwest.state )
