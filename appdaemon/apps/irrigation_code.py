8cddcd6efc614755576f814d73a0151eee41db0e
Dave-Bryant <davidcolinbryant@gmail.com>
2020-12-14 09:48:13 +1100

    Update

--- appdaemon/apps/irrigation_code.py
+++ appdaemon/apps/irrigation_code.py
@@ -39,7 +39,7 @@ class Home_Irrigation(hass.Hass):
   def main_routine(self, *args):
      self.running_time = self.render_template("{{states('sensor.smart_irrigation_daily_adjusted_run_time') | int}}")
      self.chance_of_precipitation = self.render_template("{{states('sensor.wupws_precip_chance_1d') | int}}")
-     self.log(f"Daily is: {self.running_time} seconds. Hourly is: {int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time'))} seconds. Probability of Rain: {self.chance_of_precipitation}%. Watering Threshold: {self.watering_threshold}mm. Cumulative Garden Run Time: {self.config['global_irrigation_cumulative_daily_adjusted_run_time']}mm ")
+     self.log(f"Daily is: {self.running_time} seconds. Hourly is: {int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time'))} seconds. Probability of Rain: {self.chance_of_precipitation}%. Watering Threshold: {self.watering_threshold}sec. Cumulative Garden Run Time: {self.config['global_irrigation_cumulative_daily_adjusted_run_time']}sec ")
      if int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time')) > 0 and self.running_time > self.watering_threshold and self.chance_of_precipitation < self.precipitation_threshold:
 
          self.running_time = self.running_time / self.no_of_schedules
