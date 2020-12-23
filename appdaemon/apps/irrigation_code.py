1263c19e1a25eb5bd71c5ac576d01a82aa783c68
Dave-Bryant <davidcolinbryant@gmail.com>
2020-12-24 06:18:20 +1100

    update

--- appdaemon/apps/irrigation_code.py
+++ appdaemon/apps/irrigation_code.py
@@ -39,10 +39,11 @@ class Home_Irrigation(hass.Hass):
   def main_routine(self, *args):
      self.running_time = self.render_template("{{states('sensor.smart_irrigation_daily_adjusted_run_time') | int}}")
      self.chance_of_precipitation = self.render_template("{{states('sensor.precip_chance') | int}}")
+     self.chance_of_precipitation_48hrs = self.render_template("{{states('sensor.wupws_precip_chance_2d') | int}}")
 
-     self.log(f"Daily is: {self.running_time} seconds. Hourly is: {int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time'))} seconds. Probability of Rain: {self.chance_of_precipitation}%. Probability of Rain 24hrs: {self.get_state('sensor.wupws_precip_chance_2d')}%. Watering Threshold: {self.watering_threshold}sec. ")
+     self.log(f"Daily is: {self.running_time} seconds. Hourly is: {int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time'))} seconds. Probability of Rain: {self.chance_of_precipitation}%. Probability of Rain 24hrs: {self.chance_of_precipitation_48hrs}%. Watering Threshold: {self.watering_threshold}sec. ")
 
-     if int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time')) > 0 and self.running_time > self.watering_threshold and self.chance_of_precipitation < self.precipitation_threshold and self.render_template("{{states('sensor.wupws_precip_chance_2d') | int}}") < self.precipitation_threshold:
+     if int(self.get_state('sensor.smart_irrigation_hourly_adjusted_run_time')) > 0 and self.running_time > self.watering_threshold and self.chance_of_precipitation < self.precipitation_threshold and self.chance_of_precipitation_48hrs < self.precipitation_threshold:
 
          if self.station1 != '':  # If not Garden Run then add to cumulative garden run time
              self.cumulative_total = round(self.render_template("{{states('sensor.smart_irrigation_daily_adjusted_run_time') | int}}") / self.no_of_schedules + self.render_template("{{states('input_number.garden_watering_time') | int}}"),0) #store run time for gardens
