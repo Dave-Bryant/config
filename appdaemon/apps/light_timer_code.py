8ef717d6cecba253f841943064d061fa56026471
Dave-Bryant <davidcolinbryant@gmail.com>
2022-03-24 12:04:26 +1100

    update

--- appdaemon/apps/light_timer_code.py
+++ appdaemon/apps/light_timer_code.py
@@ -19,7 +19,7 @@ class Light_Timer(hass.Hass):
      # self.run_at(self.before_sunset_cb, "09:05:00") test
   def before_sunset_cb(self, kwargs):
      if self.get_state('group.bryant_family') == 'not_home':
-         self.log("Starting Light_Timer")
+         # self.log("Starting Light_Timer")
          self.Target_Light = self.args["LIGHT_SWITCH"]
          self.log("Starting %s", self.Target_Light)
          self.flashing_light('dummy') # function needs an argument for entity_id but I cant pass it as the iteration will blank it out
@@ -28,14 +28,14 @@ class Light_Timer(hass.Hass):
          self.log("Everyone is home")
          exit
   def flashing_light(self, *args):
-     if self.get_state('sun.sun') == 'above_horizon' and self.get_state('group.bryant_family') == 'not_home':
+     if self.get_state('sun.sun') == 'below_horizon' and self.get_state('group.bryant_family') == 'not_home':
          self.duration_of_light = random.randint(3, 9) * 600 # 30-90 minutes
          self.toggle(entity_id = self.Target_Light)
          self.first_pass = True
          self.log('Light is %s for %s minutes.', self.get_state(entity_id = self.Target_Light), self.duration_of_light/60)
          self.run_in(self.flashing_light, self.duration_of_light)
      else:
-         if self.get_state('sun.sun') == 'below_horizon':
+         if self.get_state('sun.sun') == 'above_horizon':
              if self.get_state(entity_id = self.Target_Light) == 'on': self.turn_off(entity_id = self.Target_Light)
              self.log("%s has finished naturally", self.Target_Light)
          if self.get_state('sun.sun') == 'above_horizon' and self.get_state('group.bryant_family') == 'home':
