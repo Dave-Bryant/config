505948c54bd4cbf2faea6470c65d734c3576cfe1
Dave-Bryant <davidcolinbryant@gmail.com>
2023-04-11 15:47:26 +1000

    Update

--- appdaemon/apps/hello.py
+++ appdaemon/apps/hello.py
@@ -22,66 +22,54 @@ class HelloWorld(hass.Hass):
      self.log("Hello from AppDaemon")
 
 
-     self.run_in(self.main_routine, 0)
-     self.garden_run = False
+     #self.run_every(self.main_routine,"now", 15)
+     
+     #self.run_daily(self.main_routine, "9:00:00")
 
 
    # testing
 
-  def is_dst(self, when):
-    '''Given the name of Timezone will attempt determine if that timezone is in Daylight Saving Time now (DST)'''
-    return when.dst() != timedelta(0)
+   ####################################################################
 
-
-  def main_routine(self, *args):
-     #self.turn_on(entity_id='light.tz3000_tqlv4ug4_ts0001_light')
-     #self.get_state(entity_id="sensor.speedtest_ping")
-     #self.call_service("homeassistant/update_entity", entity_id = "sensor.speedtest_ping")
-     # self.call_service("tts/google_translate_say",
-     #                    entity_id = "media_player.study_display",
-     #                    message = "Your attention please, internet power cycle in 30 seconds!"
-     #                     )
-
-     #################################################
-     # Test cases
-     #################################################
-
-     # DST in Australia ends April 3, 2022 at 2 am
-     self.timezone_eastern = 'Australia/Sydney'
-     self.datetime_dst_eastern = pytz.timezone(self.timezone_eastern).localize(datetime(year=2022, month=10, day=5))
-     self.datetime_non_dst_eastern = pytz.timezone(self.timezone_eastern).localize(datetime(year=2022, month=4, day=4))
-     self.x = self.datetime_dst_eastern.strftime('%y-%m-%d %a %H:%M:%S')
-     self.y = self.datetime_non_dst_eastern.strftime('%y-%m-%d %a %H:%M:%S')
-     self.log(f'{self.timezone_eastern} {self.x} is in dst: { self.is_dst(self.datetime_dst_eastern)} should be: True')
-     self.log(f'{self.timezone_eastern} {self.y} is in dst: {self.is_dst(self.datetime_non_dst_eastern)} should be: False')
-
-     self.config["global_DST_Switch"] = self.is_dst(pytz.timezone('Australia/Sydney').localize(datetime.now()))
-
-     self.log(self.config["global_DST_Switch"])
-
-     self.nowtime = datetime.now()
-     self.target = self.nowtime.replace(hour=17, minute=50, second=0, microsecond=0)
-     if self.nowtime == self.target: self.log('quit')
-     #quit()
+  # def Check_Kilometers(self, kms, limit, multiple): # odometer, Percent tolerance , distance before notice
+  #   kms = str(round(kms/multiple,2)).split('.', 1)[1]
+  #   if int(kms) >= 100-limit:
+  #       return True
+  #   else:
+  #       return False
 
 
 
-     #self.garden_running_time = float(self.get_state('input_number.garden_watering_time'))
-     #if not self.garden_run: self.log(self.garden_running_time)
+  # def main_routine(self, *args):
+  #   self.x = 18238
+  #   tyre_check_switch = False
+    
 
-     #self.log(self.render_template("{{ states('sensor.smart_irrigation_hourly_adjusted_run_time_2') }}"))
-
-     #state = self.get_state("sensor.smart_irrigation_hourly_adjusted_run_time_2")
-     # self.log(self.get_state('sensor.smart_irrigation_base_schedule_index'))
-     # if self.get_state('sun.sun') == 'above_horizon': self.log("Works......")
-     #
-     # if float(self.get_state("sensor.smart_irrigation_hourly_adjusted_run_time")) > 360: self.log("Works......")
+  #   if tyre_check_switch and self.Check_Kilometers(self.x, 10, 10000):
+  #     print ("get tyres checked")
+  #   if not tyre_check_switch and not self.Check_Kilometers(self.x, 10, 10000):
+  #     tyre_check_switch = True
+  #     print("turned switch on")
 
 
 
-      #self.select_option("input_select.irrigation_status", "Irrigation has completed")
+    
+    #self.garden_running_time = float(self.get_state('input_number.garden_watering_time'))
+    #if not self.garden_run: self.log(self.garden_running_time)
 
-      #x = self.get_state('sensor.smart_irrigation_daily_adjusted_run_time')
-      #self.set_state('sensor.smart_irrigation_daily_adjusted_run_time', state = x, attribute = {'force_mode_duration': 15})
-      #self.set_value("input_number.garden_watering_time",self.cumulative_total) # store
-     self.log("Hello from main main_routine")
+    #self.log(self.render_template("{{ states('sensor.smart_irrigation_hourly_adjusted_run_time_2') }}"))
+
+    #state = self.get_state("sensor.smart_irrigation_hourly_adjusted_run_time_2")
+    # self.log(self.get_state('sensor.smart_irrigation_base_schedule_index'))
+    # if self.get_state('sun.sun') == 'above_horizon': self.log("Works......")
+    #
+    # if float(self.get_state("sensor.smart_irrigation_hourly_adjusted_run_time")) > 360: self.log("Works......")
+
+
+
+    #self.select_option("input_select.irrigation_status", "Irrigation has completed")
+
+    #x = self.get_state('sensor.smart_irrigation_daily_adjusted_run_time')
+    #self.set_state('sensor.smart_irrigation_daily_adjusted_run_time', state = x, attribute = {'force_mode_duration': 15})
+    #self.set_value("input_number.garden_watering_time",self.cumulative_total) # store
+    #self.log("Hello from main main_routine")
---
+++ appdaemon/apps/mercedes_check.py
@@ -0,0 +1,45 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+# App to send a message when tyres are due for realignment
+#
+
+class mercedes_tyre_check(hass.Hass):
+  def initialize(self):
+    self.log("Hello from Mercedes") 
+    self.percent_tolerance = self.args["percent_tolerance"]
+    self.distance_before = self.args["distance_before"]
+    self.msg_sent = False           
+        
+    # self.run_every(self.main_routine,"now", 15)
+    
+    self.run_daily(self.main_routine, "9:00:00")
+
+  def Check_Kilometers(self, kms, limit, multiple): # odometer, Percent tolerance , distance before notice    
+    kms = str(round(kms/multiple,2)).split('.', 1)[1]
+    if int(kms) >= 100-limit:
+        return True
+    else:
+        return False     
+
+  def main_routine(self, *args):
+    self.ODOMETER = int(self.get_state(self.args["ODOMETER"]))
+    self.log(f"Daily check on Odometer:  {self.ODOMETER}")
+    self.tyre_check_switch = self.get_state(self.args["tyre_check_switch"])
+    self.log(f"Switch is {self.tyre_check_switch}")    
+        
+    if self.tyre_check_switch == 'on' and self.Check_Kilometers(self.ODOMETER, self.percent_tolerance, self.distance_before):
+      self.notify("Reminder to get tyres checked", title = "Tyre Alignment", name = "notify")
+      self.log ("Reminder to get tyres checked")
+    if self.tyre_check_switch == 'on' and not self.Check_Kilometers(self.ODOMETER, self.percent_tolerance, self.distance_before):
+      self.turn_off(entity_id="input_boolean.check_odometer")      
+      self.log("turned switch off as service not due")
+    if self.tyre_check_switch == 'off' and not self.Check_Kilometers(self.ODOMETER, self.percent_tolerance, self.distance_before):
+      self.log("no action")
+    if self.tyre_check_switch == 'off' and self.Check_Kilometers(self.ODOMETER, self.percent_tolerance, self.distance_before):
+      if not self.msg_sent: 
+        self.turn_on(entity_id="input_boolean.check_odometer")
+        self.msg_sent = True
+        self.notify("Get tyres checked", title = "Tyre Alignment", name = "notify")      
+        self.log("Get tyres checked")
+      
+      
